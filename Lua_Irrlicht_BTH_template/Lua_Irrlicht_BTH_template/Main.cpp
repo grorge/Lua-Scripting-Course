627a747a9cb9b055bc7c361eaeb524876447f83f
Lhure <maxjonilsson@gmail.com>
2018-04-23 16:13:04 +0200

    added template from lecture

--- Lua_Irrlicht_BTH_template/Lua_Irrlicht_BTH_template/Main.cpp
+++ Lua_Irrlicht_BTH_template/Lua_Irrlicht_BTH_template/Main.cpp
@@ -28,7 +28,64 @@ public:
 	}
 };
 
+class Regex
+{
+public:
+	virtual int match(char const *)
+		= 0;
+};
+class CharClass : public Regex
+{
+public:
+	std::string contents;
+	virtual int match(char const *text);
+	CharClass(std::string c) :
+		contents(c) {}
+};
+int CharClass::match(char const *text)
+{
+	return contents.find(*text) == std::string::npos ? -1 : 1;
+}
 
+class Star : public Regex
+{
+public:
+	std::string contents;
+	Regex* operand;
+	virtual int match(char const *text);
+	Star(std::string c) :
+		contents(c) {}
+};
+int Star::match(char const *text)
+{
+	int n, consumed = 0;
+	while ((n = operand->match(text)) > 0)
+	{
+		consumed += n;
+		text += n;
+	}
+	return consumed;
+}
+
+class Seq : public Regex
+{
+public:
+	std::string contents;
+	std::list<Regex*> cells;
+	virtual int match(char const *text);
+	Seq(std::string c) :
+		contents(c) {}
+};
+int Seq::match(char const *text)
+{
+	int chars, consumed = 0;
+	for (auto c : cells) {
+		if ((chars = c->match(text)) < 0) return -1;
+		consumed += chars;
+		text += chars;
+	}
+	return consumed;
+}
 
 // Global interface to call functions from the Lua terminal
 Interface intf;
